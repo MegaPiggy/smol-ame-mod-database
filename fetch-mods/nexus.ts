d6611ffa408cd8ea8dba8d4e638984d7106c00cc
Noah Pilarski <mehmehmeh313@gmail.com>
2022-08-20 00:25:45 -0400

    delay

--- fetch-mods/nexus.ts
+++ fetch-mods/nexus.ts
@@ -1,9 +1,12 @@
 import Nexus from "@megapiggy/nexus-api";
 
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+
 export async function initializeNexus(nexusApiKey: string){
     const nexus = await Nexus.create(nexusApiKey, "Smol Ame Mods Database", "1.0.0", "smolame", 5000);
 
     return async function getNexusModInfo(id: number){
+      await delay(5000);
       return await nexus.getModInfo(id, "smolame")
     }
 }
